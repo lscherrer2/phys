8877a7932ccef10532b905555da66ae35de21101
lscherrer2 <lukescherrer2@gmail.com>
2025-07-19 11:35:19 -0700

    added electromagnetism force

--- test/test_engine.py
+++ test/test_engine.py
@@ -1,10 +1,10 @@
 from unittest import TestCase, main
-from phys import Gravity
+from phys import Gravity, Electromagnetism
 from phys import Particle
 import numpy as np
 
 class TestEngines(TestCase):
-    def test_gravitational_force (self):
+    def test_gravity (self):
         engine = Gravity(G=1.0)
         p1 = Particle(
             mass=1.0,
@@ -22,6 +22,26 @@ class TestEngines(TestCase):
         true_force = 1.0 * p1.mass * p2.mass
 
         self.assertAlmostEqual(engine_force, true_force, 5)
+
+    def test_electromagnetism (self):
+        engine = Electromagnetism(k=1.0)
+        p1 = Particle(
+            mass=1.0,
+            position=np.array([0.5, 0, 0]),
+            velocity=np.array([0, 0.5, 0]),
+            charge=1.0,
+        )
+        p2 = Particle(
+            mass=1.0,
+            position=np.array([-0.5, 0, 0]),
+            velocity=np.array([0, -0.5, 0]),
+            charge=-1.0,
+        )
+        engine_force = np.linalg.norm(engine.interact(p1, p2))
+        true_force = 1.0 * p1.charge * p2.charge
+
+        self.assertAlmostEqual(engine_force, true_force, 5)
+
 
 if __name__ == "__main__":
     main()
