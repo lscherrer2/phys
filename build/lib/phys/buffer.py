6e9df7571038b57bc96dacc806b3f80c6e33aa1a
lscherrer2 <lukescherrer2@gmail.com>
2025-07-26 17:04:41 -0700

    Delete build directory

--- build/lib/phys/buffer.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-
-class Buffer (ABC):
-    def __init__ (self, **kwargs):
-       self.buffer = {}
-
-    def flush_buffer (self):
-        for key, value in self.buffer.items():
-            setattr(self, key, value)
