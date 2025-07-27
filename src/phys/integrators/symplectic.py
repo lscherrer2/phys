9aa404d6aaa405c252f598b6c01ccec09c47db8b
lscherrer2 <lukescherrer2@gmail.com>
2025-07-27 08:23:29 -0700

    removed higher order yoshida integrators

--- src/phys/integrators/symplectic.py
+++ src/phys/integrators/symplectic.py
@@ -7,9 +7,11 @@ from copy import deepcopy
 __all__ = [
     "Leapfrog",
     "Yoshida4",
-    "Yoshida6",
-    "Yoshida8",
 ]
+
+
+def reflect_w_values(w_values: list[float]) -> list[float]:
+    return w_values + w_values[:-1][::-1]
 
 
 class Symplectic(Integrator):
@@ -51,33 +53,11 @@ class Symplectic(Integrator):
 
 
 class Leapfrog(Symplectic):
-    w_values = [
-        1.0,
-    ]
+    w_values = [1.0]
 
 
 class Yoshida4(Symplectic):
     w_values = [
         1 / (2 - 2 ** (1 / 3)),
         1 - 2 * (1 / (2 - 2 ** (1 / 3))),
-    ]
-
-
-class Yoshida6(Symplectic):
-    w_values = [
-        -1.17767998417887,
-        0.235573213359357,
-        0.784513610477560,
-    ]
-
-
-class Yoshida8(Symplectic):
-    w_values = [
-        0.311790812418427,
-        -1.55946803821447,
-        -1.67896928259640,
-        1.66335809963315,
-        -1.06458714789183,
-        1.36934946416871,
-        0.629030650210433,
     ]
